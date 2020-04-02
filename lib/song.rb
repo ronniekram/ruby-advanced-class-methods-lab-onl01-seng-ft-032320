5c4260554bbe451b33554b1a083f46a49d85acf7
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-02 01:05:55 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
-  
   @@all = []
-  
+
   def self.all
     @@all
   end
@@ -13,9 +12,9 @@ class Song
   
   def self.create
     song = self.new
-    @@all << song
+    @@all << song 
     song
-  end 
+  end
   
   def self.new_by_name(name)
     song = self.new
@@ -24,41 +23,48 @@ class Song
   end 
   
   def self.create_by_name(name)
-    song = self.new 
+    song = self.new
     song.name = name
-    @@all << song
+    @@all << song 
     song
   end
   
   def self.find_by_name(name)
-    self.all.find{|song| song.name == name}
+    @@all.find {|s| s.name == name}
+  end
+  
+  def self.find_or_create_by_name(song)
+    if self.find_by_name(song)
+      self.find_by_name(song)
+    else
+      self.create_by_name(song)
+    end
+  end
+  def self.alphabetical
+    self.all.sort_by {|song| song.name }
   end 
   
-  def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create_by_name(name)
-  end 
-  
-  def self.alphabetical()
-    self.all.sort_by{|songs| songs.name}
-  end 
-  
-  def self.new_from_filename(name)
-    song = self.new 
-    song.name = (name.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (name.split(" - ")[0])
+  def self.new_from_filename(filename)
+    filename = filename.split('.mp3').join
+    filename = filename.split(' - ')
+    song = self.new
+    song.name = filename[1]
+    song.artist_name = filename[0]
     song
-  end 
+  end
   
-  def self.create_from_filename(name)
-    song = self.new 
-    song.name = (name.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (name.split(" - ")[0])
+  def self.create_from_filename(filename)
+    filename = filename.split('.mp3').join
+    filename = filename.split(' - ')
+    song = self.new
+    song.name = filename[1]
+    song.artist_name = filename[0]
     @@all << song
     song
   end 
   
   def self.destroy_all
-    self.all.clear
+    @@all.clear
   end 
-
+  
 end
